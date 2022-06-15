952b44907f28cee8307a054d7f6dfacb1f4b3a05
Arjun259194 <arjun259194@gmail.com>
2022-06-15 22:26:53 +0530

    error in todo.js will solve later

--- js/todo.js
+++ js/todo.js
@@ -2,25 +2,37 @@ function $(selector) {
   return document.querySelector(selector);
 }
 
+function removeClass(className, element) {
+  element.classList.remove(className);
+}
+
+function addClass(className, element) {
+  element.classList.add(className);
+}
+
 //variables
 let activeList;
 
 document.querySelectorAll(".tab-button").forEach(element => {
-  element.addEventListener('click',()=>{
+  element.addEventListener('click', () => {
     setTimeout(() => {
       activeList = $('.tab-content_active');
       // console.log(activeList);  
-      activeList.querySelectorAll('.movingToDoing').forEach(e=>{
-        e.addEventListener('click',()=>{
+      activeList.querySelectorAll('.movingToDoing').forEach(e => {
+        e.addEventListener('click', () => {
           const movingTask = e.parentNode;
+          const button = movingTask.querySelector('.movingToDoing')
           e.parentNode.parentNode.removeChild(movingTask);
           $('.doing-taskList').appendChild(movingTask);
-          movingTask.classList.remove('.note-done');
-          movingTask.classList.add('.doing');
+          //!error -> classes not adding and removing 
+          removeClass('not-done', movingTask);
+          removeClass('movingToDoing',button);
+          addClass(' movingToDone',button)
+          addClass(' doing', movingTask);
         })
       })
     }, 0);
-});
+  });
 });
 
 
