98ffb0cfd0d6d7c675a34b9613bdcc1aff031ade
Arjun259194 <arjun259194@gmail.com>
2022-06-17 19:49:11 +0530

    task moving function done will fix little bugs later

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,7 @@ function $(selector) {
   return document.querySelector(selector);
 }
 
-function createTask(text,btnClassName,elementClassName) {
+function createTask(text, btnClassName, elementClassName) {
   const task = document.createElement('li');
   task.className = elementClassName;
   task.innerHTML = `<span>${text}</span><button class="${btnClassName}">✔</button>`
@@ -11,45 +11,52 @@ function createTask(text,btnClassName,elementClassName) {
   // return `<li class="doing task">${text}<button class="movingToDone">✔</button></li>`;
 }
 
+function taskManegament() {
+  activeList = $('.tab-content_active');
+  //*not-done to doing
+  activeList.querySelectorAll('.movingToDoing').forEach(e => {
+    e.addEventListener('click', () => {
+      const movingTask = e.parentNode;
+      const text = movingTask.querySelector('span').innerText;
+      console.log(text);
+      e.parentNode.parentNode.removeChild(movingTask);
+      $('.doing-taskList').appendChild(createTask(text, 'movingToDone', 'doing task'));
+      taskManegament();
+    })
+  })
+
+  //*doing to done
+  activeList.querySelectorAll('.movingToDone').forEach(e => {
+    e.addEventListener('click', () => {
+      const movingTask = e.parentNode;
+      const text = movingTask.querySelector('span').innerText;
+      console.log(text);
+      e.parentNode.parentNode.removeChild(movingTask);
+      $('.done-taskList').appendChild(createTask(text, 'movingToNotDone', 'done task'));
+      taskManegament();
+    })
+  })
+
+  //*done to not-done
+  activeList.querySelectorAll('.movingToNotDone').forEach(e => {
+    e.addEventListener('click', () => {
+      const movingTask = e.parentNode;
+      const text = movingTask.querySelector('span').innerText;
+      console.log(text);
+      e.parentNode.parentNode.removeChild(movingTask);
+      $('.not-done-taskList').appendChild(createTask(text, 'movingToDoing', 'not-done task'));
+      taskManegament();
+    })
+  })
+}
+
 //variables
 let activeList;
 
 document.querySelectorAll(".tab-button").forEach(element => {
   element.addEventListener('click', () => {
     setTimeout(() => {
-      activeList = $('.tab-content_active');
-      //*not-done to doing
-      activeList.querySelectorAll('.movingToDoing').forEach(e => {
-        e.addEventListener('click', () => {
-          const movingTask = e.parentNode;
-          const text = movingTask.querySelector('span').innerText;
-          console.log(text);
-          e.parentNode.parentNode.removeChild(movingTask);
-          $('.doing-taskList').appendChild(createTask(text,'movingToDone','doing task'));
-        })
-      })
-
-      //*doing to done
-      activeList.querySelectorAll('.movingToDone').forEach(e => {
-        e.addEventListener('click', () => {
-          const movingTask = e.parentNode;
-          const text = movingTask.querySelector('span').innerText;
-          console.log(text);
-          e.parentNode.parentNode.removeChild(movingTask);
-          $('.done-taskList').appendChild(createTask(text,'movingToNotDone','done task'));
-        })
-      })
-
-      //*done to not-done
-      activeList.querySelectorAll('.movingToNotDone').forEach(e => {
-        e.addEventListener('click', () => {
-          const movingTask = e.parentNode;
-          const text = movingTask.querySelector('span').innerText;
-          console.log(text);
-          e.parentNode.parentNode.removeChild(movingTask);
-          $('.not-done-taskList').appendChild(createTask(text,'movingToDoing','not-done task'));
-        })
-      })
+      taskManegament();
     }, 0);
   });
 });
